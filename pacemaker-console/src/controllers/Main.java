137bd8d3bcc84984987e0ede4395522bbbb7c35c
jamie sheehan <jamie sheehan@10.3.32.37>
2016-10-05 09:36:31 +0100

    Reworking users to use a map interface.

--- pacemaker-console/src/controllers/Main.java
+++ pacemaker-console/src/controllers/Main.java
@@ -1,7 +1,9 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import models.User;
 
@@ -15,7 +17,7 @@ public class Main
     pacemakerAPI.createUser("Homer", "Simpson", "homer@simpson.com", "secret");
     pacemakerAPI.createUser("Lisa",  "Simpson", "lisa@simpson.com",  "secret");
 
-    List<User> users = pacemakerAPI.getUsers();
+    Collection<User> users = pacemakerAPI.getUsers();
     System.out.println(users);
   }
 }
